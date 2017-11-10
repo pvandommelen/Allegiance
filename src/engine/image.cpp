c500bc9222ae1cdfab6b1f1a62aa031374be79a4
Peter van Dommelen <pvandommelen@users.noreply.github.com>
2017-11-10 14:59:17 +0100

    Fixes issue with calculating image bounds.

--- src/engine/image.cpp
+++ src/engine/image.cpp
@@ -123,7 +123,7 @@ TRef<Surface> ConstantImage::GetSurface()
 
 void ConstantImage::Render(Context* pcontext)
 {
-    pcontext->DrawImage(m_psurface);
+    pcontext->DrawImage3D(m_psurface, Color(1, 1, 1));
 }
 
 ZString ConstantImage::GetString(int indent)
@@ -696,6 +696,7 @@ void WrapImage::SetImage(Image* pvalue)
 
 void WrapImage::CalcBounds()
 {
+    GetImage()->CalcBounds();
     m_bounds = GetImage()->GetBounds();
 }
 
